c5800dec88b37a40e62b6bd6f156bcccc4820629
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-06-01 07:52:57 -0300

    feat: adicionando menu de visualização e biblioteca para adicionar formataçã UTF-8 no terminal

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+#include <windows.h>
+
+void utf8_terminal();
+void menu();
+
 int main() {
+    utf8_terminal();
+
+    menu();
     return 0;
+}
+
+void utf8_terminal() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+}
+
+void menu() {
+    printf("\n==== SISTEMA DE MONITORAMENTO ====\n");
+    printf("1 - Inserir Dados\n");
+    printf("2 - Visualizar Status\n");
+    printf("3 - Executar Análise\n");
+    printf("4 - Encerrar Sistema\n");
 }
